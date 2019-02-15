1bacbcb10b0167ef83c880c67c3424b0e72ad499
minorhash <minorhash@gmail.com>
2019-02-15 20:13:58 +0900

    02/15/19

--- vie/index.jsx
+++ vie/index.jsx
@@ -3,30 +3,52 @@ var DefLay=require("./lay/def")
 
 class Hell extends React.Component {
 
-crNum(){
-
+Week1(){
+const pre= [];
+for (var i=24;i<29;i++){
+pre.push(<span>{i}</span>)
+}
+const lspre=pre.map((nr) =>
+<span  key={nr.toString()}>{nr}&nbsp;&nbsp;</span>
+);
 const nums= [];
-    for (var i=1;i<32;i++){
-        nums.push(<span>{i}</span>)
-    }
+for (var i=1;i<3;i++){
+nums.push(<span>{i}</span>)
+}
+
+//const res=nums.splice(2,1)
+//const thr=<a href="item3">{res}</a>
+    //nums.splice(2,0,<span id="bkg1">{thr}</span>)
+const ls=nums.map((nr) =>
+<span  key={nr.toString()}>&nbsp;&nbsp;{nr}&nbsp;&nbsp;</span>
+);
+
+return(
+<span id="bkg1">{lspre}{ls}</span>
+);
+}
+
+Week2(){
+const nums= [];
+for (var i=3;i<10;i++){
+nums.push(<span>{i}</span>)
+}
 
 const res=nums.splice(2,1)
 const thr=<a href="item3">{res}</a>
     nums.splice(2,0,<span>{thr}</span>)
-
 const ls=nums.map((nr) =>
-<span key={nr.toString()}>{nr}&nbsp;&nbsp;</span>
+<span key={nr.toString()}>&nbsp;&nbsp;{nr}&nbsp;&nbsp;</span>
 );
 
-
-
 return(
-<span>{ls}</span>
+<span id="bkg1">{ls}</span>
 );
 }
 
 render() {
 return (
+
 <DefLay title={this.props.title}>          
 <div className="container-fluid">
 <div className="row">
@@ -41,10 +63,12 @@ return (
     </div>
 {/* row */}
     <div className="row">
-<div className="col bg-bei p-3">
+
+<div className="col bg-bei p-3" >
 
 <div align="center">bye {this.props.name}</div>
-{this.crNum()}
+{this.Week1()}<br/>
+{this.Week2()}
 </div>
 <div className="col bg-ali">
 </div>
--- vie/lay/def.jsx
+++ vie/lay/def.jsx
@@ -1,18 +1,19 @@
 var React = require('react');
 
 class DefLay extends React.Component {
-  render() {
-    return (
-      <html>
-          <head>
+render() {
+return (
+<html>
+<head>
 <link rel="stylesheet" href="/css/boot.css" />
-              <title>{this.props.title}</title></head>
-              <body>
-                  {this.props.children}
-              </body>
-      </html>
-    );
-  }
+<link rel="stylesheet" href="/css/bas.css" />
+<title>{this.props.title}</title></head>
+<body>
+{this.props.children}
+</body>
+</html>
+);
+}
 }
 
 module.exports = DefLay;
